db58aa36cb08214c37bc2aaa98fff48582ca6c80
Vincent Portelli <vinport16@gmail.com>
2017-02-12 13:30:42 -0500

    fixed DHT I think probably

--- drivers/humidity_driver.c
+++ drivers/humidity_driver.c
@@ -1,10 +1,14 @@
 #include "drivers.h"
-#include "DHT.h" 
+#include <DHT.h> // you also need adafruit_sensors.h, it's included in DHT.h
 
-manual: https://cdn-learn.adafruit.com/downloads/pdf/dht.pdf*/
+//////////////////////manual: https://cdn-learn.adafruit.com/downloads/pdf/dht.pdf*/
+
+//here is where to find actually useful info -->  https://github.com/adafruit/DHT-sensor-library/blob/master/examples/DHTtester/DHTtester.ino
+
 void humidity_setup (int pin)
 {
   Serial.begin(9600);
+  DHT dht(pin, DHT11);
   dht.begin();
 }
 /*apparently takes 250ms to read humidity*/
@@ -12,7 +16,7 @@ float get_humidity (void)
 {
   dht.readHumidity(); 
 }
-/*returns temp in farenheight*/
-float get_temperature(void){
-  dht.readTemperature(true);
+/*returns temp in farenheight or celcius*/
+float get_temperature(bool farenheight){
+  dht.readTemperature(farenheight);  //default is Celcius, readTemperature(true) gives Farenheight
 }
