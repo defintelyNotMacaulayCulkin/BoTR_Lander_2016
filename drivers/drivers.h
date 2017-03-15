88dcebea65aae07ee2ba7993e702288d3f762216
Justin Green <electrojustin@gmail.com>
2017-03-15 13:08:50 -0400

    Started fleshing out the protocol for camera data transmissions

--- drivers/drivers.h
+++ drivers/drivers.h
@@ -17,7 +17,10 @@ void light_setup (int pin);
 int get_light (void);
 
 void camera_setup (int pin1, int pin2);
-void get_image (char* buf);
+void camera_read (void (*callback)(char*, int, char, void*), void* parameter); //Parameters for the callback function: byte buffer, number of bytes in buffer, whether this is the last readout or not, user provided parameter
+									       //The camera sends data 32 bytes at a time. Every 32 bytes, the callback function is called. The last read may not be the full 32 bytes, that's what the second parameter is for.
+									       //The bool is whether or not this is the last time the callback will be called for this current read loop. Useful for control packet fragmentation, etc.
+									       //The last parameter of the callback is the same as the last parameter of the camera_read function. We probably won't be using this for sending camera data over the network, but this is helpful for e.g. saving to an SD card. You can use this parameter to specify the filename
 
 void xbee_setup (int pin1, int pin2);
 void xbee_send (char* buf, int len);
