a1e7f293047b3912c14510462bbb0926117e158c
defintelyNotMacaulayCulkin <jte28@case.edu>
2017-03-19 16:15:13 -0400

    Update light_driver.cpp

--- drivers/light_driver.cpp
+++ drivers/light_driver.cpp
@@ -1,9 +1,13 @@
 #include "drivers.h"
 
-void light_setup (int pin)
+void light_setup (void)
 {
+  pinMode(A3, INPUT);
 }
 
 int get_light (void)
 {
+  float val = (float) analogRead(A3);
+  float percent = (val/1024)*100;
+  return percent;
 }
